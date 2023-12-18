53e736718322d4de001a32d18d915c2d31d5542d
FuhadSanin <fuhadsanin500@gmail.com>
2023-12-19 01:08:30 +0530

    /api/v1/restaurants removed

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ import ReviewDAO from "./dao/reviewDAO.js"
 
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 3001
+const port = process.env.PORT
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use("/api/v1/restaurants", restaurantRoutes)
+app.use("/", restaurantRoutes)
 app.use("*", (req, res) => {
   res.status(404).send("Error")
 })
